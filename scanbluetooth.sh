542d8e95326aa697fe0fa70bc428b17ca70d3288
crazypapa-cl <crazypapaa@gmail.com>
2024-06-11 01:43:40 -0400

    scan

---
+++ scanbluetooth.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+adapter_info=$(bluetoothctl show)
+echo "Información del adaptador Bluetooth:"
+echo "$adapter_info"
+echo "-------------------------------------"
+
+# Habilitar el adaptador Bluetooth
+bluetoothctl power on
+
+# Configurar el adaptador para ser descubrible y conectable
+bluetoothctl discoverable on
+bluetoothctl pairable on
+
+# Iniciar el escaneo
+bluetoothctl scan on &
+
+# Esperar 10 segundos para escanear los dispositivos
+sleep 10
+
+# Detener el escaneo
+bluetoothctl scan off
+
+# Listar los dispositivos encontrados
+devices=$(bluetoothctl devices)
+
+# Mostrar los dispositivos encontrados
+echo "Dispositivos Bluetooth encontrados:"
+echo "$devices"
