c9194346096eaea718f95262fe7128e1c0b56f7d
crazypapa-cl <62182046+crazypapa-cl@users.noreply.github.com>
2024-06-11 22:10:47 -0400

    Stop

--- scanbluetooth.sh
+++ scanbluetooth.sh
@@ -1,8 +1,34 @@
 #!/bin/bash
 
+# Nombre del adaptador Bluetooth (alias)
+nuevo_nombre="CRAZYLAB"
+
+# Función para manejar señales de terminación
+cleanup() {
+    echo "Deteniendo el escaneo..."
+    bluetoothctl scan off
+    exit 0
+}
+
+# Atrapar señales de interrupción y terminación
+trap cleanup SIGINT SIGTERM
+
+# Obtener información del adaptador Bluetooth
 adapter_info=$(bluetoothctl show)
+
+# Mostrar información del adaptador Bluetooth
 echo "Información del adaptador Bluetooth:"
 echo "$adapter_info"
+echo "-------------------------------------"
+
+# Cambiar el nombre del adaptador Bluetooth
+echo "Cambiando el nombre del adaptador Bluetooth a: $nuevo_nombre"
+bluetoothctl system-alias "$nuevo_nombre"
+
+# Verificar el cambio de nombre
+nuevo_adapter_info=$(bluetoothctl show)
+echo "Información actualizada del adaptador Bluetooth:"
+echo "$nuevo_adapter_info"
 echo "-------------------------------------"
 
 # Habilitar el adaptador Bluetooth
